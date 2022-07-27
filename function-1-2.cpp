43f3f6e68eae3fc4a8c64ef508bc14e260072424
Caleb Tonkin <caleb.tonkin1@gmail.com>
2022-07-27 11:52:55 +0930

    Final code for question 1-2

---
+++ function-1-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+
+double array_mean(int array[], int n){
+    int sum=0;
+    int mean=0;
+    if (n<1){
+        return 0.0;
+    }
+    for (int i=0; i<n; i++){
+    sum = sum + array[i];
+    }
+    mean = sum/n;
+    return mean;
+
+}
