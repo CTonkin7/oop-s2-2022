26e35916fbbf3ddeee98c024d0124f573c9435dc
Caleb Tonkin <caleb.tonkin1@gmail.com>
2022-07-27 14:44:52 +0930

    question 3

---
+++ function-3-1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+bool is_fanarray(int array[], int n){
+    bool result = true;
+    
+    if (n<0){
+        result = false;
+        return result;
+   
+    
+    return result;
+    }
+}
---
+++ main-3-1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+// The `extern` keyword tells the compiler that the count function exists,
+// but will be implemented somewhere else
+extern bool bool is_fanarray(int array[], int n);
+
+int main() {
+   
+}
