00a00c48cca7db372d9d40699939e3f5c0984ba6
CTonkin7 <104712550+CTonkin7@users.noreply.github.com>
2022-07-29 16:18:40 +0930

    Update function-3-4.cpp

--- function-3-4.cpp
+++ function-3-4.cpp
@@ -4,19 +4,19 @@ void print_pass_fail(char grade){
 
     switch(grade) {
   case 'A':
-    std::cout << "pass\n" << std::endl;
+    std::cout << "Pass\n" << std::endl;
     break;
   case 'B':
-    std::cout << "pass\n" << std::endl;
+    std::cout << "Pass\n" << std::endl;
     break;
   case 'C':
-    std::cout << "pass\n" << std::endl;
+    std::cout << "Pass\n" << std::endl;
     break;
   case 'D':
     std::cout << "fail\n" << std::endl;
     break;
   case 'E':
-    std::cout << "fail\n" << std::endl;
+    std::cout << "Fail\n" << std::endl;
     break;
 default:
     std::cout << "Nothing\n" << std::endl;
