b7753a3313171a0691278b9d3103d041cfa13131
Caleb Tonkin <caleb.tonkin1@gmail.com>
2022-08-02 11:25:24 +0930

    Question 1-1 finished

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 
-extern int is_identity(int array[4][4]);
+extern int sum_diagonal(int array[4][4]);
 
 int main(){
     int array = {1,2,3,4;5,6,7,8;9,10,11,12;13,14,15,16};
-    std:cout << is_identity(array) << std:endl;
+    std::cout << "The sum of the diagonal is: " << sum_diagonal(array) << std::endl;
     
-
 }
