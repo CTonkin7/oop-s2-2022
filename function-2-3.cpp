eb326a3016be54ec566e0ce88bb8ba8f017b3b71
Caleb Tonkin <caleb.tonkin1@gmail.com>
2022-08-02 22:31:27 +0930

    question 2-3

--- function-2-3.cpp
+++ function-2-3.cpp
@@ -1,13 +1,59 @@
 #include <iostream>
 
-bool is_palindrome(int integers[], int length);
+extern bool is_palindrome(int integers[], int length);
+extern int sum_if_palindrome(int integers[], int length);
+extern int sum_array_elements(int integers[], int length);
 
 
 
+int sum_if_palindrome(int integers[], int length){
+    int sum = 0;
+    if(is_palindrome(integers,length)!=0){
+        sum = sum_array_elements(integers,length);
+    } else {
+        std::cout << -2 << std::endl;
+    }
+    return sum;
+}
 
-int sum_if_palindrome(int integers[], int length);
+bool is_palindrome(int integers[], int length){
+    bool result = true;
+    
+    if (length<=0){
+        result = false;
+        return result;
+    }
+    if (length % 2 == 1){
+        for (int i = 1; i<(length/2+1);i++){
+            if (integers[i]<integers[i-1]){
+                return false;
+            }
+        }     
+    } else {
+        for (int i = 1; i<(length/2);i++){
+            if (integers[i]<integers[i-1]){
+                return false;
+            }
+        }    
+    }
+    for (int i =0; i<length;i++){
+        if (integers[i] != integers[length-i-1]){
+            return false;
+        }
+    }
+    return result;
+}
 
-
-
-
-int sum_array_elements(int integers[], int length);
+int sum_array_elements(int integers[], int length){
+    int sum = 0;
+    int result = 0;
+    if (length<=0){
+        result = -1;
+        return result;
+    }
+    for (int i=0; i<length;i++){
+        sum = sum + integers[i];
+    }
+    std::cout << sum << std::endl;
+    return sum;
+}
--- main-2-3.cpp
+++ main-2-3.cpp
@@ -6,16 +6,7 @@ extern int sum_array_elements(int integers[], int length);
 
 int main(){
    int integers[5] = {1,2,3,2,1};
-
-   if (is_palindrome(integers, 5) != -2){
-        sum_if_palindrome(integers, 5);
-        sum_array_elements(integers,5);
-   }
-
-   
-
-   
-
-
+   sum_if_palindrome(integers,5);
+   return 0;
 
 }
