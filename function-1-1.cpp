be73a9f4580f32347f96de07a17dafe1a084d617
Caleb Tonkin <caleb.tonkin1@gmail.com>
2022-08-10 14:07:47 +0930

    changes

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -3,7 +3,10 @@
 void print_matrix(int array[10][10]){
     for (int i=0; i<10;i++){
         for (int j=0; j<10;j++){
-            std::cout << array[i][j] << " ";
+            std::cout << array[i][j];
+            if (i!=9){
+                std::cout << " ";
+            }
         }  
         std::cout << "" << std::endl;
     }
