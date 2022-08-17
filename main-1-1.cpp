0515e057d0aa33b7bc8e6c05fc1d6da8931396c1
Caleb Tonkin <caleb.tonkin1@gmail.com>
2022-08-17 11:54:33 +0930

    question-1-1

---
+++ main-1-1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+extern void string_2d_copy(std::string first[][2], std::string second[][2], int n);
+
+int main(){
+    int n = 3;
+    string first[3][2] = { {"a","b"}, {"c","d"}, {"e","f"}};
+    string second[3][2];
+    string_2d_copy(first,second,n);
+
+}
