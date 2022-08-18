83f4f3548bc56186f7ae31c3c488e5c9665c58ef
Caleb Tonkin <caleb.tonkin1@gmail.com>
2022-08-18 16:35:58 +0930

    changes

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -9,7 +9,7 @@ void string_2d_copy(std::string first[][2], std::string second[][2], int n){
     }
     for (int i=0; i<n;i++){
         for(int j=0;j<2;j++){
-            std::cout << first[i][j] << " ";
+            std::cout << second[i][j] << " ";
         }
         std::cout<< "" << std::endl;
     }
--- main-1-1.cpp
+++ main-1-1.cpp
@@ -5,7 +5,7 @@ extern void string_2d_copy(std::string first[][2], std::string second[][2], int 
 
 int main(){
     int n = 3;
-    string first[3][2] = { {"a","b"}, {"c","d"}, {"e","f"}};
+    string first[3][2] = {{"a","b"}, {"c","d"}, {"e","f"}};
     string second[3][2];
     string_2d_copy(first,second,n);
 
