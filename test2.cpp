f466e44442985d73e54aec0b7640d1f8fce8b073
Caleb Tonkin <caleb.tonkin1@gmail.com>
2022-08-24 12:05:55 +0930

    test2

---
+++ test2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int max_sub_sum(int *nums,int length){
+    int maxsum = 0;
+    int *ptr_array = nums;
+    int index = 1;
+
+    for (int i=0; i<length;i++){
+        if (ptr_array < 0){
+            *(ptr_array+1);
+        } else {
+            index = 0;
+        }
+    }
+
+    for (int i=0; i<length; i++){
+        for (int j=0; j<length; j++){
+            int sum = 0;
+            for (int k = i; k < (j+1); k++){
+                sum = sum + *(ptr_array+k);
+            }
+            if(sum>maxsum){
+                maxsum=sum;
+            }
+        }
+    }
+    return maxsum;
+}
