5c0f31edeec6ee798fe7754f56f57d574a23d55f
Caleb Tonkin <caleb.tonkin1@gmail.com>
2022-08-24 14:09:06 +0930

    changes

--- main-4-1.cpp
+++ main-4-1.cpp
@@ -10,4 +10,5 @@ int main(){
     int row3[3]={23,56,1};
     int *vals[]={row1,row2,row3};
     int *max_min = matrix_min_max(vals,nrows,ncols);
+    cout << *(max_min) << *(max_min +1) << endl;
 }
