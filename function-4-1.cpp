789fa3a68afc5f6809d3569609e184984c1c7c79
Caleb Tonkin <caleb.tonkin1@gmail.com>
2022-08-24 14:33:24 +0930

    changes

--- function-4-1.cpp
+++ function-4-1.cpp
@@ -1,25 +1,29 @@
 #include <iostream>
+using namespace std;
 
 int* matrix_min_max(int **vals,int num_rows, int num_cols){
     int **ptr = vals;
-    std::cout<< **ptr <<std::endl;
+    //std::cout<< **ptr <<std::endl;
     int max = **ptr;
     int min = **ptr;
     //(ptr++);
     //std::cout<< **ptr <<std::endl;
     for (int i=0; i< num_rows; i++){
         for (int j=0; j< num_cols;j++){
-            if (**(ptr) > max ){
-                max = **(ptr);
-                (ptr++);
-            } else if((**(ptr) < min)){
-                min = **(ptr); 
-                (ptr++);
+            if (**ptr > max ){
+                max = **ptr;
+            } else if((**ptr < min)){
+                min = **ptr; 
             }
+            (ptr+i);
         }
     }
+    cout << max << endl;
+    cout << min << endl;
     int *max_min = new int[2];
+
     *max_min = min;
     *(max_min++) = max;
+    
     return max_min;
 }
