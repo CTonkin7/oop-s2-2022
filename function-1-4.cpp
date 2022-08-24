f4288281a18a1d84b09351b884cda30df7ab68e1
Caleb Tonkin <caleb.tonkin1@gmail.com>
2022-08-24 13:58:06 +0930

    changes

--- function-1-4.cpp
+++ function-1-4.cpp
@@ -14,7 +14,7 @@ int* matrix_min_max(int **vals,int num_rows, int num_cols){
             (**ptr + 1);
         }
     }
-    new int[2] = int *max_min;
+    new int[2] = int max_min;
     max_min = {max,min};
     return max_min;
 }
--- main-1-4.cpp
+++ main-1-4.cpp
@@ -5,9 +5,9 @@ extern int* matrix_min_max(int **vals,int num_rows, int num_cols);
 int main(){
     int nrows = 3;
     int ncols = 3;
-    int row1[]={10,15,99};
-    int row2[]={100,5,73};
-    int row3[]={23,56,1};
+    int row1[3]={10,15,99};
+    int row2[3]={100,5,73};
+    int row3[3]={23,56,1};
     int *vals[]={row1,row2,row3};
     int *max_min = matrix_min_max(vals,nrows,ncols);
 }
