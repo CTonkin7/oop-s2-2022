d4b74d53487a47f2c9b7bca9428c11c27a25070b
Caleb Tonkin <caleb.tonkin1@gmail.com>
2022-09-07 12:45:20 +0930

    changes

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
 #include <string>
-
+#include "function.h"
 using namespace std;
     
 int main(){
-    
+    Musician musician1("violin", 3);
+
+    cout << musician1.get_instrument() << endl;
+    cout << musician1.get_experience() << endl;
+
+    Musician musician2();
+
+    cout << musician2.get_instrument() << endl;
+    cout << musician2.get_experience() << endl;
+
+    Musician musician3("guitar", 10);
+
+    cout << musician3.get_instrument() << endl;
+    cout << musician3.get_experience() << endl;
 
 
 }
