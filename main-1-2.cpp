1ed8ddeb51cb44a0062f2fccae2c286255e1ec80
Caleb Tonkin <caleb.tonkin1@gmail.com>
2022-09-07 14:22:58 +0930

    changes

--- main-1-2.cpp
+++ main-1-2.cpp
@@ -5,20 +5,19 @@
 using namespace std;
 
 int main(){
-    
     Cage Cage1("blake", 2345);
     Cage Cage2("Jack", 3245);
     Cage Cage3("Harley", 7488);
     
     Clinic Clinic1("vet", 5);
     
-    Clinic add_cage(Cage Cage1); 
-    Clinic add_cage(Cage Cage2); 
-    Clinic add_cage(Cage Cage3); 
+    Clinic1.add_cage(Cage1); 
+    Clinic1.add_cage(Cage2); 
+    Clinic1.add_cage(Cage3); 
 
-    Clinic get_number_of_cages();
+    Clinic1.get_number_of_cages();
 
-    Cage get_cages();    
+    Clinic1.get_cages();    
 
 }
 
